cf708aa64427f0a693efdd58dc2fac95fb5c749d
aglander <glander@me.com>
2025-02-03 16:39:42 +0100

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,7 +4,7 @@ const Welcome = () => {
       <div className="bg-white/80 rounded-lg shadow-lg p-8">
         <h1 className="text-3xl font-serif mb-6">Herzlich Willkommen auf meiner persönlichen Webseite!</h1>
         <p className="text-lg mb-4">
-          Hier können Sie mich in Bild, Ton und Wort kennenlernen.
+          Hier können Sie mich in Bild und Wort kennenlernen.
         </p>
         <p className="text-lg mb-8">
           Ich würde mich freuen, wenn die Webseite Ihnen hilft, all jene Informationen über meine bisherige, gegenwärtige und zukünftige Arbeit zu finden, die Sie, verehrter Besucher, benötigen.
